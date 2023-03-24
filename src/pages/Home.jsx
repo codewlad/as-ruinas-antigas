cd1e061c24dee7133f3b65a71cfee4b8b7997ffc
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-03-24 10:53:43 -0300

    First commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+function Home() {
+  return (
+    <div id="container">
+      
+      <header>
+        <div>
+          <img class="imgLogo" src="" alt="Logo" />
+          <img class="imgTitle" src="../src/img/as-ruinas-antigas.png" alt="As Ruínas Antigas" />
+        </div>
+        <div>
+          <nav>
+            <ul>
+              <li>O Jogo</li>
+              <li>Imagens</li>
+              <li>Mecânicas</li>
+              <li>Jogar/Download</li>
+            </ul>
+          </nav>
+        </div>        
+      </header>
+
+      <main>
+        <div id="theGame">
+          <h2>O Jogo</h2>
+          <p>Conteúdo</p>
+        </div>
+        <div id="images">
+          <h2>Imagens</h2>
+          <p>Conteúdo</p>
+        </div>
+        <div id="mechanics">
+          <h2>Mecânicas</h2>
+          <p>Conteúdo</p>
+        </div>
+        <div id="playDownload">
+          <h2>Jogar / Download</h2>
+          <p>Conteúdo</p>
+        </div>
+      </main>
+      
+    </div>    
+  )
+}
+
+export default Home
