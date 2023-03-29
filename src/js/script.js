3f80e990df54bfaec09fb57c0243a07ea74d0791
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-03-29 11:49:58 -0300

    Fixed links to CSS and images

--- src/js/script.js
+++ src/js/script.js
@@ -11,9 +11,7 @@ function posicao(e) {
     function moveToDive(a) {
         const divID = document.getElementById(a).id
         const divHeight = document.getElementById(a).clientHeight
-        //console.log(`DivID ${divID}: tem ${divHeight} de altura`)
         divClicked = document.getElementById(e).id
-        //console.log(`Div clicada: ${divClicked}`)
 
         if(divID === divClicked) {
             position = cont 
@@ -23,72 +21,10 @@ function posicao(e) {
             position = 20
         }
         if(position < 20){
-            totalHeight += divHeight + 20
+            totalHeight += divHeight + 19.9
         }
         cont++
-    }
-    
-    /*
-    e = e.target.id.substring(4).toLowerCase()
-    var el = document.getElementById(e);
-    var coordenadas = el.getBoundingClientRect();
-    console.log('posição x', coordenadas.left, 'posição y', coordenadas.top)
-    //window.scrollTo(coordenadas.left, coordenadas.top)
-    console.log(coordenadas.top)
-    console.log(el)
-    if(coordenadas.top == 125) {
-        window.scrollTo(coordenadas.left, coordenadas.top-125)
-    } else {
-        window.scrollTo(coordenadas.left, coordenadas.top)
-    }
-    */
-    
-
-    /*
-    const headerPadding = 125
-    const divArray = ["thegame", "images", "mechanics", "playdownload"]
-
-    //console.log(divArray)
-
-    var totalHeight = 0, position = 0, cont = 0
-    divArray.map(moveToDive)
-    
-    function moveToDive(a) {
-
-        // get height
-        const divHeight = document.getElementById(a).clientHeight
-
-        // check div
-        var el = e.target.id.substring(4).toLowerCase()
-        var el_ = document.getElementById(el);
-        var coordenadas = el_.getBoundingClientRect();
-
-        console.log(`A: ${a}`)
-        console.log(`EL: ${el}`)
-        
-        if(a === el) {
-            position = cont 
-            //
-            totalHeight += headerPadding
-            var coordenadas = el.getBoundingClientRect();
-            window.scrollTo(coordenadas.left, totalHeight)
-            //
-            position = 20          
-        }
-        if(position < 20)(
-            totalHeight += divHeight
-        )
-        console.log(`Cont: ${cont}`)
-        console.log(`Posição: ${position}`)
-        cont ++
-        
-        
-        
-        //e = e.target
-        //var el = document.getElementById(e);
-        //console.log(el)
-    }*/
-    
+    }    
 }
 
 document.getElementById('linkTheGame').addEventListener('click', posicao);
